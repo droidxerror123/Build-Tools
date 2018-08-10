7d5fba1a059560bf83fba4315eb102cda822b126
Droid <39285561+droidxerror123@users.noreply.github.com>
2018-08-10 07:57:26 +0000

    Create Home.sh

---
+++ Home.sh
@@ -0,0 +1,285 @@
+#!/bin/bash
+#############################
+# Mau ReCode?? Mikir Goblok #
+#############################
+
+#############################
+# Kalo Mau ReCode IZIN Dulu #
+# Whatsapp : +6289606402413 #
+#############################
+clear
+
+
+echo "\033[92mWellcome To My Tools\033[0m"
+sleep 0.1
+figlet "  Build.Tools"
+echo """
+     \033[93m[\033[91m*\033[93m] \033[92mAuthor  \033[91m: \033[96m./Droid aka Fahri
+     \033[93m[\033[91m*\033[93m] \033[92mContact \033[91m: \033[96m+6289606402413
+     \033[93m[\033[91m*\033[93m] \033[92mDate    \033[91m: \033[96m10-08-2018
+     \033[93m[\033[91m*\033[93m] \033[92mGithub  \033[91m: \033[96mhttps://github.com/droidxerror123
+"""
+echo """\033[92m
+ini adalah tools yg berisi \n\033[91mcara pembuatan tools Installer"
+sleep 0.1
+echo "\033[93mIni Hanya Untuk Pembelajaran"
+sleep 1
+echo "\033[96m"
+ echo "1) Build Tools 1"
+ echo "2) Build Tools 2"
+ echo "3) Build Tools 3"
+ echo "4) Build Tools 4"
+ echo "5) Build Tools 5"
+ echo "6) Gabung Semua Tools"
+ echo "0) EXIT" 
+ read -p "root@Pilih=>"  ea ;
+ 
+ if [ $ea = 1 ]
+ then
+ clear
+ echo "Masukan Perintah Toolsnya" | lolcat
+echo "\033[93mex:pkg install git php python2"
+read -p "=>" D ;
+read -p "=>" E ;
+read -p "=>" F ;
+read -p "=>" G ;
+echo "Github Toolsnya\n\033[96mex: git clone https://github.com/droidxerror123/BotFB\033[0m"
+read -p "=>" H ;
+echo """
+$D
+$E
+$F
+$G
+$H """ >> Tools-1.sh
+fi
+read -p "[Y / N ] :" Y ;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sh Home.sh
+fi
+
+if [ $Y = N ] || [ $Y = n ]
+then
+clear
+exit
+fi
+
+if [ $ea = 2 ]
+ then
+ clear
+ echo "Masukan Perintah Toolsnya" | lolcat
+echo "\033[93mex:pkg install git php python2"
+read -p "=>" D ;
+read -p "=>" E ;
+read -p "=>" F ;
+read -p "=>" G ;
+echo "Github Toolsnya\n\033[96mex: git clone https://github.com/droidxerror123/BotFB"
+read -p "=>" H ;
+echo """
+$D
+$E
+$F
+$G
+$H """ >> Tools-2.sh
+fi
+read -p "[Y / N ] :" Y ;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sh Home.sh
+fi
+
+if [ $Y = N ] || [ $Y = n ]
+then
+clear
+exit
+fi
+
+if [ $ea = 3 ]
+ then
+ clear
+  echo "Masukan Perintah Toolsnya" | lolcat
+echo "\033[93mex:pkg install git php python2"
+read -p "=>" D ;
+read -p "=>" E ;
+read -p "=>" F ;
+read -p "=>" G ;
+echo "Github Toolsnya\n\033[96mex: git clone https://github.com/droidxerror123/T.DYF"
+read -p "=>" H ;
+echo """
+$D
+$E
+$F
+$G
+$H """ >> Tools-3.sh
+fi
+
+read -p "[Y / N ] :" Y ;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sh Home.sh
+fi
+
+if [ $Y = N ] || [ $Y = n ]
+then
+clear
+exit                                                                
+fi
+
+if [ $ea = 4 ]
+ then
+ clear                                                               echo "Masukan Perintah Toolsnya" | lolcat
+echo "\033[93mex:pkg install git php python2"
+read -p "=>" D ;
+read -p "=>" E ;
+read -p "=>" F ;
+read -p "=>" G ;
+echo "Github Toolsnya\n\033[96mex: git clone https://github.com/droidxerror123/T.DEF-09"
+read -p "=>" H ;
+echo """
+$D
+$E
+$F
+$G
+$H """ >> Tools-4.sh
+fi
+
+read -p "[Y / N ] :" Y ;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sh Home.sh
+fi
+
+if [ $Y = N ] || [ $Y = n ]
+then
+clear
+exit                                                                
+fi
+
+if [ $ea = 5 ]
+ then
+ clear
+  echo "Masukan Perintah Toolsnya" | lolcat
+echo "\033[93mex:pkg install git php python2"
+read -p "=>" D ;
+read -p "=>" E ;
+read -p "=>" F ;
+read -p "=>" G ;
+echo "Github Toolsnya\n\033[96mex: git clone https://github.com/droidxerror123/T.HUNTER"
+read -p "=>" H ;
+echo """
+$D
+$E
+$F
+$G
+$H """ >> Tools-5.sh
+fi
+
+read -p "[Y / N ] :" Y ;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sh Home.sh
+fi
+
+if [ $Y = N ] || [ $Y = n ]
+then
+clear
+exit                                                                
+fi
+
+if [ $ea = 6 ]
+then
+clear
+echo "Nama Tools-1"
+read -p "=>" I ;
+echo "Nama Tools-2"
+read -p "=>" J ;
+echo "Nana Tools-3"
+read -p "=>" K ;
+echo "Nana Tools-4"
+read -p "=>" L ;
+echo "Nama Tools-5"
+read -p "=>" M ; 
+echo "Masukan Nick Anda" | lolcat
+read -p "=>" A ;
+echo "Masukan Nama Team" | lolcat
+read -p "=>" B ;
+echo "Masukan Contact WA" | lolcat
+read -p "=>" C ;
+
+echo """
+#!/bin/bash
+clear
+figlet 'My-Tools'
+echo ' | Author = $A'
+echo ' | Team   = $B'
+echo ' | Contact= $C'
+echo ''
+echo 'Wellcome To MY TOOLS'
+echo '1) $I'
+echo '2) $J'
+echo '3) $K'
+echo '4) $L'
+echo '5) $M'
+read -p 'pilih=>' apa ;
+
+if [ $apa = 1 ]
+then
+cat Tools-1.sh
+fi
+
+if [ $apa = 2 ]
+then
+clear
+cat Tools-2
+fi
+
+if [ $apa = 3 ]
+then
+clear
+cat Tools-3
+fi
+
+if [ $apa = 4 ]
+then
+clear
+cat Tools-4
+fi
+
+if [ $apa = 5 ]
+then
+clear
+cat Tools-5
+fi
+""" >> Gabung.sh
+fi
+read -p "[Y / N ] :" Y ;
+
+if [ $Y = Y ] || [ $Y = y ]
+then
+clear
+sh Home.sh
+fi
+
+if [ $Y = N ] || [ $Y = n ]
+then
+clear
+exit                                                                
+fi
+
+if [ $ea = 0 ]
+then
+clear
+echo "Terima Kasih"
+echo "Sanpai Bertemu Lain Waktu\nThanks To My Friends"
+exit
+fi
